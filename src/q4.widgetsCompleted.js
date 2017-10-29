0a2ae5f8aeaa259205eb994ed0efcbc386830dfc
Flo <infloent@yahoo.com>
2017-10-29 21:34:58 +0200

    updated $.extend with 'true' parameter for deep copy

---
+++ src/q4.widgetsCompleted.js
@@ -0,0 +1,54 @@
+/**
+ * Adds the functioanlity to run a callback function when all the choosen <code>q4.widgets</code> have completed.
+ * 
+ * @version 1.0.0
+ * 
+ * @class  q4.widgetsCompleted
+ * @example 
+ * q4Defaults.widgetsCompleted({
+ *       widgets: [{
+ *           $element: $('.module-financial-quarter .module_container--inner'),
+ *           events: "financialscomplete"
+ *       }, {
+ *           $element: $('.module-financial-year'),
+ *           events: "financialscomplete"
+ *       }],
+ *       completed: function(events) {
+ *           console.log(this);
+ *           console.dir(events);
+ *       }
+ *  });
+ */
+$.extend(true, q4Defaults, /** @lends q4.widgetsCompleted */ {
+    /**
+     * Runs a callback function when all the choosen <code>q4.widgets</code> have completed.
+     * 
+     * @param [obj] {object}  object literal that will be used as an paramenter when calling <code>'widgetsCompleted'</code> method. 
+     * <br><br>
+     * This object has the following properties and methods:
+     * @param [completed] {function} Callback f
+     * @param [widgets] {array<objects>}  <br><br>Array of object containing two properties:
+     * 
+     * @param [$element] {element} Element on which a <code>q4.widget</code> was initialized
+     * @param [events] {event} Name of the event triggered by the <code>q4.widget</code> when completed.
+     * 
+     */
+    widgetsCompleted: function(obj) {
+        var widgetsPromises = [];
+        $.each(obj.widgets, function(i, widget) {
+            var def = new $.Deferred();
+
+            widget.$element.on(widget.events, function(event) {
+                def.resolve(event);
+            });
+
+            widgetsPromises.push(def);
+        });
+        // apply widgetsPromises array as arguments to $.when method
+        $.when.apply($, widgetsPromises).then(function() {
+            //call 'completed' callback which will receive as a parameter the arguments array from '.then' calback function method 
+            obj.completed(arguments);
+        });
+    }
+});
+
