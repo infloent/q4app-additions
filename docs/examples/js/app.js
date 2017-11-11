284b88dc2939d8f35f273b71c2d876a485080a54
Flo <infloent@yahoo.com>
2017-11-11 03:56:17 +0200

    changes nothing fancy

--- docs/examples/js/app.js
+++ docs/examples/js/app.js
@@ -1,11 +1,18 @@
-$('.nav--main').stickyWidget();
-$('.nav--secondary').stickyWidget({
+$('.pane--header').stickyWidget({
+    triggerOffset: -50,
+    triggerOffsetElemHeight: "negative",
+    positionOffsetElemHeight: "negative",
+    classes: {
+        "js_sticky-elem": "js--sticky-slide",
+        "js--sticky": "js--sticky-slide-active"
+    },
+});
+$('.nav--secondary' ).stickyWidget({
     offsetTopElem: [$('.nav--main')],
     offsetTopBreakPoint: [],
-    disableStickyBreakPoint: 1024,
-    disableStickySwitchCondition: true,
-    stickyElem: "append",
+    disableBreakPoint: 1024,
+    disableSwitchCondition: true,
+    stickyElem: ".level2",
     layoutStickyActiveCls: "js--nav--secondary-sticky",
     disabled: false
 });
-
