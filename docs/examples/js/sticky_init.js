9c43c4fd337f4d81e6a69fa376eb912663ffa214
inflonet <infloent@yahoo.com>
2017-11-16 19:42:28 +0200

    added _calcTriggerOffset method and refactored the 'triggerOffsetAdd' and 'triggerOffsetRemove' mehods accordingly

--- docs/examples/js/sticky_init.js
+++ docs/examples/js/sticky_init.js
@@ -1,17 +1,17 @@
 $('.pane--header').stickyWidget({
-    disableBreakPoint: 1024,
-    disableSwitchCondition: true,
     afterSticky: function(event) {
         // console.log(event);
     }
 });
-$('.pane--navigation').stickySlide({
-    offsetTopElem: [$('.nav--main')],
+$('.pane--navigation').stickyWidget({
+    offsetTopElem: [$('.pane--header')],
     offsetTopBreakPoint: [],
     disableBreakPoint: 1024,
     disableSwitchCondition: true,
     layoutStickyActiveCls: "js--nav--secondary-sticky",
-    // showHide: true,
-    triggerOffset: 0,
-
+    // triggerOffsetAddSticky: -300,
+    // triggerOffsetRemoveSticky: -150,
+    // triggerOffsetElemHeightAddSticky: "negative",
+    // triggerOffsetElemHeightRemoveSticky: "positive",
 });
+
